a0e0bbc8c86681e2248e7367bdb5b2a884a6efb8
itw-smart <tobias@demokratie.today>
2021-01-25 17:08:32 +0100

    proj

--- app/controllers/custom/proposals_controller.rb
+++ app/controllers/custom/proposals_controller.rb
@@ -2,6 +2,8 @@ require_dependency Rails.root.join("app", "controllers", "proposals_controller")
 
 class ProposalsController
 
+  before_action :process_tags, only: [:create, :update]
+
   def index_customization
     discard_draft
     discard_archived
@@ -13,11 +15,24 @@ class ProposalsController
   end
 
   private
-  def take_only_by_tag_names
-    if params[:tags].present?
-      @resources = @resources.tagged_with(params[:tags].split(","), all: true)
-      @subcategories = @resources.tag_counts.subcategory
+    def process_tags
+      params[:proposal][:tag_list_categories].split(",").each do |t|
+        next if t.strip.blank?
+        Tag.find_or_create_by name: t.strip, kind: :category
+      end
+      params[:proposal][:tag_list_subcategories].split(",").each do |t|
+        next if t.strip.blank?
+        Tag.find_or_create_by name: t.strip, kind: :subcategory
+      end
+      params[:proposal][:tag_list] ||= ""
+      params[:proposal][:tag_list] += ((params[:proposal][:tag_list_categories] || "").split(",") + (params[:proposal][:tag_list_subcategories] || "").split(",")).join(",")
+      params[:proposal][:tag_list_categories], params[:proposal][:tag_list_subcategories] = nil, nil
     end
-  end
-  
+
+    def take_only_by_tag_names
+      if params[:tags].present?
+        @resources = @resources.tagged_with(params[:tags].split(","), all: true)
+        @subcategories = @resources.tag_counts.subcategory
+      end
+    end
 end
